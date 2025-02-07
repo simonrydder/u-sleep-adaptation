5f36e91a981adb32c6f5db6234204760d6c254d4
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-07 13:25:33 +0100

    fixing in progress

--- minimal_example.py
+++ minimal_example.py
@@ -1,14 +1,28 @@
 import os
 
+from lightning import LightningModule
 from src.csdp_pipeline.factories.dataloader_factory import USleep_Dataloader_Factory
 from src.csdp_training.lightning_models.factories.lightning_model_factory import (
     USleep_Factory,
 )
+from src.usleep.usleep import USleep
+
+
+class LightningSleep(LightningModule):
+    def __init__(self) -> None:
+        super().__init__()
+        self.usleep = USleep()
+
+
+class Test(LightningModule):
+    def __init__(self) -> None:
+        super().__init__()
+        self.pretrained = LightningSleep.load_from_checkpoint()
 
 
 def main():
     dataloader = USleep_Dataloader_Factory(
-        gradient_steps=100,
+        gradient_steps=886,
         batch_size=64,
         hdf5_base_path=os.path.join("data", "hdf5"),
         trainsets=["eesm19"],
@@ -17,11 +31,18 @@ def main():
         data_split_path=os.path.join("data", "splits", "test.json"),
     )
 
-    pass
-    test_loader = dataloader.create_testing_loader(1)
+    usleep_fac = USleep_Factory(0.0001, 64, 5, 0.5, 10, 2)
+    model = usleep_fac.create_pretrained_net(
+        os.path.join("src", "usleep", "weights", "Depth10_CF05.ckpt")
+    )
 
-    model = USleep_Factory(0.0001, 64, 10, 0.5, 2)
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    import torch
+
+    print(torch.__version__)  # Check PyTorch version
+    print(torch.cuda.is_available())  # Should print True
+    print(torch.cuda.device_count())  # Should print number of GPUs
+    print(torch.cuda.get_device_name(0))  # Print GPU name
